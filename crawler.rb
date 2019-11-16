410691cbe0fd090af669f6e17f5c75631e6768c8
bigfaiber <bigfaiber@github.com>
2019-11-15 21:45:45 -0500

    more features

--- crawler.rb
+++ crawler.rb
@@ -16,11 +16,14 @@ require 'webdrivers'
 	@TRM = 0
 	def get_usd_trm
 		p 'calculando trm'
+		a =  Time.now
 		page = scrape_page(SETFX_USD_TRM_URL)
-		browser = Watir::Browser.start('http://www.set-fx.com/index.html')
+		browser = Watir::Browser.start('http://www.set-fx.com/index.html', browser = :chrome, headless: true)
 		Watir::Wait.until(timeout: 300) {browser.element(css: "#barraTuristaPromedio").text.include? '$'}
-		trm = browser.element(css: "#barraTuristaPromedio").text[2..].delete(',').to_f
 		p 'trm calculada'
+		b = Time.now
+		p "#{b - a} seconds"
+		trm = browser.element(css: "#barraTuristaPromedio").text[2..].delete(',').to_f
 	end
 		
 	# end
@@ -28,10 +31,10 @@ require 'webdrivers'
 
 
 	def get_quick_sell_offers
-    page = scrape_page(LOCAL_BITCOINS_COP_QUICK_SELL_URL)
+		page = scrape_page(LOCAL_BITCOINS_COP_QUICK_SELL_URL)
     ofertas = page.css('td.column-price').map{|o| o.children.text.strip[0...-4].delete(',').to_f/@TRM}
   end
-p
+
   def get_quick_buy_offers
     page = scrape_page(LOCAL_BITCOINS_USD_QUICK_BUY_URL)
     ofertas = page.css('td.column-price').map{|o| o.children.text.strip[0...-4].delete(',').to_f}
@@ -41,7 +44,9 @@ p
 		@TRM = get_usd_trm
 		cop_offers = get_quick_sell_offers
 		usd_offers = get_quick_buy_offers
-		byebug
+		p "Best offer in COP today is #{cop_offers.first}"
+		p "Best offer in USD today is #{usd_offers.first}"
+		p "Spread for today is: #{((usd_offers.first/cop_offers.first).round(2)-1)*100}%"
 	end
 	
 	# private 
